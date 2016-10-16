5b00ba0cfa4b922b7940b03a2f26f1a3123a8e93
XFL <18720909751@163.com>
2016-10-16 10:07:40 +0800

    增加一个quartz任务

--- src/main/java/com/xfl/schedule/task/QuartzTest2JobBean.java
+++ src/main/java/com/xfl/schedule/task/QuartzTest2JobBean.java
@@ -6,25 +6,19 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.scheduling.quartz.QuartzJobBean;
 
-
 /**
  * Created by XFL.
- * time on 2016/10/4 11:40
- * description:定时任务
+ * time on 2016/10/15 16:42
+ * description:定时任务2
  */
-public class QuartzTestJobBean extends QuartzJobBean {
+public class QuartzTest2JobBean extends QuartzJobBean {
     /**
      * 日志打印.
      */
     private final Logger log = LoggerFactory.getLogger(QuartzTestJobBean.class);
-    /**
-     * 测试定时任务.
-     * @param jobExecutionContext 定时任务
-     * @throws JobExecutionException 抛出异常
-     */
     @Override
-    protected final void executeInternal(final JobExecutionContext jobExecutionContext) throws JobExecutionException {
-        log.info("执行定时任务1");
+    protected void executeInternal(JobExecutionContext jobExecutionContext) throws JobExecutionException {
+        log.info("执行定时任务2");
         System.out.println("执行定时任务");
     }
 }
