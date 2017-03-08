1bd269fb2ef349a784ffd0f3e4a5c228119a445b
XFL <18720909751@163.com>
2017-03-08 23:59:19 +0800

    注释优化

--- src/test/java/com/test/RestTester.java
+++ src/test/java/com/test/RestTester.java
@@ -7,14 +7,14 @@ import org.springframework.web.client.RestTemplate;
 import org.springframework.web.util.UriComponentsBuilder;
 
 /**
- * Created by XFL
+ * Created by XFL.
  * time on 2017/3/8 23:37
  * description:测试工具类
  */
 public class RestTester {
     private final String url;
 
-    private final MultiValueMap<String, String> params = new LinkedMultiValueMap<String, String>();
+    private final MultiValueMap<String, String> params = new LinkedMultiValueMap<>();
 
     public void set(String key, String value) {
         params.add(key, value);
@@ -31,26 +31,24 @@ public class RestTester {
     }
 
     /**
-     * 发送get请求
+     * 发送get请求.
      *
      * @return 返回请求结果
      */
     public ResponseEntity<String> get() {
         String fullUrl = UriComponentsBuilder.fromHttpUrl(url).queryParams(params).build().toUriString();
         RestTemplate restTemplate = new RestTemplate();
-        ResponseEntity<String> responseEntity = restTemplate.getForEntity(fullUrl, String.class);
-        return responseEntity;
+        return restTemplate.getForEntity(fullUrl, String.class);
     }
 
     /**
-     * 发送post请求
+     * 发送post请求.
      *
      * @return 返回请求结果
      */
     public ResponseEntity<String> post() {
         String fullUrl = UriComponentsBuilder.fromHttpUrl(url).build().toUriString();
         RestTemplate restTemplate = new RestTemplate();
-        ResponseEntity<String> responseEntity = restTemplate.postForEntity(fullUrl, params, String.class);
-        return responseEntity;
+        return restTemplate.postForEntity(fullUrl, params, String.class);
     }
 }
