42a2a64233122020431714cb3e61aba6df8177a2
XFL <18720909751@163.com>
2017-03-12 19:57:57 +0800

    测试方法修改，将返回的json转化为JavaBean

--- src/test/java/com/test/TestGet.java
+++ src/test/java/com/test/TestGet.java
@@ -1,6 +1,9 @@
 package com.test;
 
 
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.xfl.common.entity.Response;
+
 /**
  * Created by XFL
  * time on 2017/3/8 23:41
@@ -10,7 +13,14 @@ public class TestGet {
     public static void main(String[] args) {
         RestTester restTester = new RestTester("http://localhost:8080/Test");
         String result = restTester.get(String.class);
-        System.out.println(result);
+        ObjectMapper objectMapper = new ObjectMapper();
+        try {
+            Response response = objectMapper.readValue(result, Response.class);
+            System.out.println(response.getData());
+        } catch (Exception e) {
+            System.out.println("Error");
+        }
+
     }
 
 }
