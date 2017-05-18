8917f2a4e57822f00f0357468ae7c0df7c67db2d
XFL <18720909751@163.com>
2017-05-18 20:48:43 +0800

    参数命名修正

--- src/main/java/com/xfl/common/utils/MD5FileUtil.java
+++ src/main/java/com/xfl/common/utils/MD5FileUtil.java
@@ -17,7 +17,7 @@ import java.security.NoSuchAlgorithmException;
  */
 public class MD5FileUtil {
     public static final long SIZE_15M = 15 * 1024 * 1024;
-    private static char hexDigits[] = {'0', '1', '2', '3', '4', '5', '6', '7', '8', '9', 'a', 'b', 'c', 'd', 'e', 'f'};
+    private static char[] hexDigits = {'0', '1', '2', '3', '4', '5', '6', '7', '8', '9', 'a', 'b', 'c', 'd', 'e', 'f'};
     private static MessageDigest messagedigest = null;
 
     static {
